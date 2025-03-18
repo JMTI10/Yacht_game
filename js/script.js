f6f76de3b5e05c8a72eef79a050b78a88616b1ac
JMTI10 <idg@live.com.pt>
2025-03-18 12:54:56 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.getElementById("rollDice").addEventListener("click", function() {
     const diceContainer = document.getElementById("diceContainer");
-    diceContainer.innerHTML = ""; // Clear previous dice
-    rollAllDice(diceContainer, 5); // Call function from diceroller.js
+    diceContainer.innerHTML = "";
+    rollAllDice(diceContainer, 5);
 });
